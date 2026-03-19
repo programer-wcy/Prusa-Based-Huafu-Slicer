cf3230a734784dcb614911731a3dfc6d4500b322
yxy654321 <www.0808.com@163.com>
2026-03-19 14:23:23 +0800

    copyright by hfll

---
+++ huafu_slicer/build/src/libslic3r/libslic3r_version.h
@@ -0,0 +1,9 @@
+#ifndef __SLIC3R_VERSION_H
+#define __SLIC3R_VERSION_H
+
+#define SLIC3R_APP_NAME "Slicer"//PrusaSlicer
+#define SLIC3R_APP_KEY "PrusaSlicer"
+#define SLIC3R_VERSION "2.9.4"
+#define SLIC3R_BUILD_ID "PrusaSlicer-2.9.4+UNKNOWN"
+
+#endif /* __SLIC3R_VERSION_H */
